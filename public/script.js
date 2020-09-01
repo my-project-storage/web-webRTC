fab25b1a5d7651cd7640d41f804247c4e04b2a8e
bbaktaeho <lth9036@gmail.com>
2020-09-01 16:13:40 +0900

    save

--- public/script.js
+++ public/script.js
@@ -1,6 +1,13 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const errorDiv = document.createElement('div');
 const myVideo = document.createElement('video');
+const peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
+});
+
 // 기본 벙어리 설정
 myVideo.muted = true;
 
@@ -16,16 +23,42 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', (call) => {
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', (userId) => {
+      connectToNewUser(userId, stream);
+    });
   })
   .catch((err) => {
     errorDiv.innerText = '웹 캠을 찾을 수 없어요!';
     videoGrid.append(errorDiv);
   });
 
+// ! socket
+peer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+// ! custom function
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
 };
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { debug: true });
 
+// set or middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+// routes
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
+});
+
+// * 소켓 연결
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId); // 그룹에 입장
+    socket.to(roomId).broadcast.emit('user-connected', userId); // 나를 제외한 그룹 전체에 전달
+  });
 });
 
 server.listen(3030, () => {
