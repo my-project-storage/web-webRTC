1f3c30839a2831953e807c79f52b3053e70eed53
bbaktaeho <lth9036@gmail.com>
2020-09-01 19:32:38 +0900

    Fix: Server

--- server.js
+++ server.js
@@ -1,36 +1,42 @@
 const express = require('express');
 const app = express();
+// const cors = require('cors')
+// app.use(cors())
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, { debug: true });
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+const { v4: uuidV4 } = require('uuid');
 
-// set or middleware
+app.use('/peerjs', peerServer);
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-// routes
-app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
+
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
 
-// * 소켓 연결
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId); // 그룹에 입장
-    socket.to(roomId).broadcast.emit('user-connected', userId); // 나를 제외한 그룹 전체에 전달
-
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
     socket.on('message', (message) => {
-      io.to(roomId).emit('create-message', message);
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
     });
   });
 });
 
-server.listen(3030, () => {
-  console.log('server start');
-});
+server.listen(process.env.PORT || 3030);
