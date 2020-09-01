480b67368163831d160ddcc854d27c220393f44d
bbaktaeho <lth9036@gmail.com>
2020-09-01 17:36:28 +0900

    Add: Styling

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
-const socket = io("/");
-const videoGrid = document.getElementById("video-grid");
-const errorDiv = document.createElement("div");
-const myVideo = document.createElement("video");
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const errorDiv = document.createElement('div');
+const myVideo = document.createElement('video');
 const peer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "3030",
+  path: '/peerjs',
+  host: '/',
+  port: '3030',
 });
 
 // 기본 벙어리 설정
@@ -24,41 +24,48 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
-    peer.on("call", (call) => {
+    peer.on('call', (call) => {
       call.answer(stream);
-      const video = document.createElement("video");
-      call.on("stream", (userVideoStream) => {
+      const video = document.createElement('video');
+      call.on('stream', (userVideoStream) => {
         addVideoStream(video, userVideoStream);
       });
     });
 
-    socket.on("user-connected", (userId) => {
+    socket.on('user-connected', (userId) => {
       connectToNewUser(userId, stream);
     });
   })
   .catch((err) => {
-    errorDiv.innerText = "웹 캠을 찾을 수 없어요!";
-    videoGrid.append(errorDiv);
+    addNoVideo();
   });
 
 // ! socket
-peer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id);
+peer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id);
 });
 
 // ! custom function
+// ? 함수의 위치가 달라졌을 때 왜 작동을 안 하는지 확인
 const connectToNewUser = (userId, stream) => {
   const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
+  const video = document.createElement('video');
+  call.on('stream', (userVideoStream) => {
     addVideoStream(video, userVideoStream);
   });
 };
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
+  video.addEventListener('loadedmetadata', () => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+const addNoVideo = () => {
+  const div = document.createElement('div');
+  div.innerText = '캠 없어요';
+  div.style.cssText = 'text-align:center; height: 300px;width: 400px;object-fit: cover; background-color: green';
+  videoGrid.append(div);
 };
